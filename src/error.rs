56adb578a60bb72e283ce60181fc33e43830f231
Junji Takakura <j.takakura@gmail.com>
2025-10-19 17:22:40 +0900

    Fix: Replace err-derive with thiserror, update error.rs, and resolve all Rust warnings
    Signed-off-by: Junji Takakura <j.takakura@gmail.com>

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,6 @@
 /// Contains an error message passed by Game Music Emu
-#[derive(Debug, err_derive::Error)]
-#[error(display = "_0")]
+#[derive(Debug, thiserror::Error)]
+#[error("{0}")]
 pub struct GmeError(String);
 
 impl GmeError {
@@ -16,12 +16,24 @@ impl GmeError {
 pub(crate) type GmeResult<T> = Result<T, GmeError>;
 
 /// Either an IO error or a GME error
-#[derive(Debug, err_derive::Error)]
+#[derive(Debug, thiserror::Error)]
 pub enum GmeOrIoError {
-    #[error(display = "IO error: _0")]
+    #[error("IO error: {0}")]
     IoError(#[source] std::io::Error),
-    #[error(display = "GME error: _0")]
+    #[error("GME error: {0}")]
     Gme(#[source] GmeError),
+}
+
+impl From<std::io::Error> for GmeOrIoError {
+    fn from(err: std::io::Error) -> Self {
+        GmeOrIoError::IoError(err)
+    }
+}
+
+impl From<GmeError> for GmeOrIoError {
+    fn from(err: GmeError) -> Self {
+        GmeOrIoError::Gme(err)
+    }
 }
 
 // pub(crate) type GmeOrIoResult<T> = Result<T, GmeOrIoError>;
